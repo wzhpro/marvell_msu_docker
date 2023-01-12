13e483d44c27c3a54baec9c38f9c4dcb2485c6d6
Air <root@wzh.me>
2023-01-12 14:31:54 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM fedora:31
+RUN yum install -y \
+	unzip \
+	libxcrypt-compat \
+	libnsl \
+	procps \
+	wget \
+	dash
+
+COPY entrance.sh .
+# https://download.lenovo.com/servers/mig/2019/04/11/19912/mrvl_utl_msu_4.1.10.2046_linux_x86-64.tgz 
+COPY MSU-4.1.10.2046-1.x86_64.rpm .
+RUN yum install -y MSU-4.1.10.2046-1.x86_64.rpm
+
+# redirect config db.xml
+RUN mv /opt/marvell/storage/db/db.xml /opt/marvell/storage/db/db.xml.orig && \
+	ln -s /etc/marvell/db.xml /opt/marvell/storage/db/db.xml
+
+# redirect logs from raid controller
+RUN rm /opt/marvell/storage/db/mvraidsvc.log && \
+	ln -s /dev/stdout /opt/marvell/storage/db/mvraidsvc.log
+
+
+EXPOSE 8845/tcp
+
+ENTRYPOINT ["/entrance.sh"]
---
+++ entrance.sh
@@ -0,0 +1,34 @@
+#!/bin/dash
+
+# set root password if given 
+if [ -n "$MSU_ROOT_PASSWD" ]
+then
+	echo "root:$MSU_ROOT_PASSWD" | chpasswd
+fi
+
+# create config folders
+if [ ! -d /etc/marvell/cli ]
+then
+	mkdir /etc/marvell/cli
+	touch /etc/marvell/cli/mvcli.ini
+fi
+
+# create db.xml
+if [ ! -f /etc/marvell/db.xml ]
+then
+	cp /opt/marvell/storage/db/db.xml.orig /etc/marvell/db.xml
+fi
+
+if [ "$1" = "cli" ]
+then
+	shift
+	exec /opt/marvell/storage/cli/mvcli "$@"
+fi
+
+
+# start storage agent in background
+/opt/marvell/storage/svc/MarvellStorageAgent start
+
+# start web server in foreground
+export LD_LIBRARY_PATH=/opt/marvell/storage/apache2/lib64:/opt/marvell/storage/expat/lib64:/opt/marvell/storage/libxml2/lib64:/opt/marvell/storage/openssl/lib64:/opt/marvell/storage/php/lib64
+exec /opt/marvell/storage/apache2/bin/apachectl -D FOREGROUND
